c1348337b72e4ac5b5795ee28ab1b34f35b4debb
lom-ali <lomali1999999@mail.ru>
2023-08-08 10:40:19 +0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "./app/store";
+import Headers from "./Components/Header/Headers";
+import Footer from "./Components/Footer";
+import About from "./Components/Personal/About";
+import Office from "./Components/Personal/Office";
+import News from "./Components/Personal/News";
+import Contacts from "./Components/Personal/Contacts";
+import Blog from "./Components/Personal/Blog";
 
 
 function App() {
@@ -17,15 +24,22 @@ function App() {
   return (
     <>
       <Header />
+      <Headers/>
       <Container>
         <Routes>
           {token ? (
             <>
+
               <Route path="/sign-up" element={<Navigate to={"/"} />} />
               <Route path="/sign-in" element={<Navigate to={"/"} />} />
               <Route path="/chat" element={<Chat />} />
               <Route path="/" element={<Home />} />
               <Route path="*" element={<Navigate to={"/"} />} />
+              <Route path="/office" element={<Office/>}/>
+              <Route path="/about" element={<About/>}/>
+              <Route path="/news" element={<News/>}/>
+              <Route path="/contacts" element={<Contacts/>}/>
+              <Route path="/blog" element={<Blog/>}/>
             </>
           ) : (
             <>
@@ -37,7 +51,8 @@ function App() {
             </>
           )}
         </Routes>
-      </Container>
+      </Container>  
+      <Footer/>
     </>
   );
 }
